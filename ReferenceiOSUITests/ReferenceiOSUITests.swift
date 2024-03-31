6c3258d010c40f15f700d0ca9de9b5b5a58be975
yulee20 <yuliyale@ukr.net>
2024-03-31 21:52:17 +0200

    initial project

---
+++ ReferenceiOSUITests/ReferenceiOSUITests.swift
@@ -0,0 +1,21 @@
+//
+//  ReferenceiOSUITests.swift
+//  ReferenceiOSUITests
+//
+//  Created by Dunya Kirkali on 27/03/2019.
+//  Copyright © 2019 ABN AMRO. All rights reserved.
+//
+
+import XCTest
+
+class ReferenceiOSUITests: XCTestCase {
+
+    override func setUp() {
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+
+    func testGenerate() {
+
+    }
+}
